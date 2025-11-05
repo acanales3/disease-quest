53e07c9f7f1a4874b8b308173190567aa26ee9a2
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-11-05 21:35:24 +0100

    Add icons and reduce duplication

--- app/components/NavBars/AdminNavBar.vue
+++ app/components/NavBars/AdminNavBar.vue
@@ -1,12 +1,27 @@
 <template>
   <nav class="flex flex-col space-y-2 p-4">
-    <NuxtLink to="/admin/dashboard" exact class="block px-3 py-2 rounded hover:bg-white transition hover:text-black">Dashboard</NuxtLink>
-    <NuxtLink to="/admin/instructors" class="block px-3 py-2 rounded hover:bg-white transition hover:text-black">Instructors</NuxtLink>
-    <NuxtLink to="/admin/students" exact class="block px-3 py-2 rounded hover:bg-white transition hover:text-black">Students</NuxtLink>
-    <NuxtLink to="/admin/classrooms"exact class="block px-3 py-2 rounded hover:bg-white transition hover:text-black">Classrooms</NuxtLink>
-    <NuxtLink to="/admin/cases" exact class="block px-3 py-2 rounded hover:bg-white transition hover:text-black">Cases</NuxtLink>
-    <NuxtLink to="/admin/analytics" exact class="block px-3 py-2 rounded hover:bg-white transition hover:text-black">Analytics</NuxtLink>
-    <NuxtLink to="/admin/leaderboard" exact class="block px-3 py-2 rounded hover:bg-white transition hover:text-black">Leaderboard</NuxtLink>
+    <NuxtLink
+      v-for="link in links"
+      :key="link.to"
+      :to="link.to"
+      exact
+      class="flex items-center gap-2 px-3 py-2 rounded hover:bg-white transition hover:text-black"
+      active-class="bg-white text-black"
+    >
+      <Icon :name="link.icon" class="text-xl"/>
+      <span>{{ link.label }}</span>
+    </NuxtLink>
   </nav>
 </template>
 
+<script setup>
+const links = [
+  { label: 'Dashboard', to: '/admin/dashboard', icon: 'iconamoon:home-light' },
+  { label: 'Instructors', to: '/admin/instructors', icon: 'hugeicons:teacher' },
+  { label: 'Students', to: '/admin/students', icon: 'hugeicons:students' },
+  { label: 'Classrooms', to: '/admin/classrooms', icon: 'simple-icons:googleclassroom' },
+  { label: 'Cases', to: '/admin/cases', icon: 'si:book-line' },
+  { label: 'Analytics', to: '/admin/analytics', icon: 'uim:analytics' },
+  { label: 'Leaderboard', to: '/admin/leaderboard', icon: 'material-symbols:social-leaderboard-outline-rounded' },
+]
+</script>
--- app/layouts/student.vue
+++ app/layouts/student.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div class="flex flex-col min-h-screen p-4 bg-gray-100">
     <!-- Main area: sidebar + content -->
-    <div class="flex flex-1">
+    <div class="flex flex-1 gap-4">
       <!-- Sidebar -->
-      <aside class="w-64 bg-blue-600 text-white flex flex-col">
-        <NavBarsAdminNavBar class="flex-1 overflow-y-auto p-4" />
+      <aside class="w-64 bg-[#AF67F0] text-white flex flex-col border-2 rounded-xl shadow-md">
+        <h1 class="text-2xl font-bold text-center py-4">
+          DiseaseQuest
+        </h1> 
+        <NavBarsStudentNavBar class="flex-1 overflow-y-auto p-4" />
       </aside>
 
       <!-- Main content -->
