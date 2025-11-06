3a1640790d563212eaddf6e0dee8c49c0f1d4525
Chris Musselman <chrismusselman@Chriss-MacBook-Air.local>
2025-11-06 15:15:01 -0600

    fixed type safety for column filtering in /admin/cases and /student/cases

--- app/components/views/student/cases.vue
+++ app/components/views/student/cases.vue
@@ -9,15 +9,16 @@ import type { Case } from "../../CaseDatatable/columns";
 import { onMounted, ref, computed } from "vue";
 import { columns } from "../../CaseDatatable/columns";
 import DataTable from "../../CaseDatatable/data-table.vue";
-import { cases} from "~/assets/interface/Case.ts"
+import { cases} from "~/assets/interface/Case"
 
 const data = ref<Case[]>([]);
 
 const visibleColumns = computed(() => {
   const columnsToShow = ['id', 'name', 'description', 'classroom', 'completionDate', 'status', 'actions'];
-  return columns.filter(column => 
-    columnsToShow.includes(String(column.id || column.accessorKey))
-  );
+  return columns.filter(column => {
+    const key = 'id' in column ? column.id : 'accessorKey' in column ? column.accessorKey : undefined;
+    return key ? columnsToShow.includes(String(key)) : false;
+  });
 });
 
 async function getData(): Promise<Case[]> {
