d5c28aee6eddacfe56097e943d72091c8fc3fb08
acanales3 <alexander.s.canales@gmail.com>
2025-11-06 15:38:20 -0600

    SCRUM-127: finished datatable component for classrooms

--- app/components/views/instructor/classrooms.vue
+++ app/components/views/instructor/classrooms.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>
-    <h1>Classrooms</h1>
+  <div class="container py-2 mx-auto">
+    <DataTable :columns="visibleColumns" :data="data" />
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import type { Classroom } from "../../ClassroomDatatable/columns";
+import { onMounted, ref } from "vue";
+import { columns } from "../../ClassroomDatatable/columns";
+import DataTable from "../../ClassroomDatatable/data-table.vue";
+import { classrooms } from "../../../assets/interface/Classroom";
 
+const data = ref<Classroom[]>([]);
+
+const visibleColumns = computed(() => {
+  return columns.map((col) => {
+    const columnsToShow = [
+      "id",
+      "name",
+      "code",
+      "section",
+      "startDate",
+      "endDate",
+      "status",
+    ];
+
+    return {
+      ...col,
+      meta: {
+        ...col.meta,
+        hidden: !columnsToShow.includes(col.accessorKey as string),
+      },
+    };
+  });
+});
+
+async function getData(): Promise<Classroom[]> {
+  // Fetch data from your API here.
+  return classrooms;
+}
+
+onMounted(async () => {
+  data.value = await getData();
+});
 </script>
