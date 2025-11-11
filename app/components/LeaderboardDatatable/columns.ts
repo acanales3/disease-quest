4b457d2262fdcedebfae98aaec72b432a93ab457
Dylan Noall <dylannoall@outlook.com>
2025-11-10 23:02:00 -0600

    FEAT: created Leaderboard component and attached it to each user type's leaderboard routing. Admin's see an extra column with the student's name

--- app/components/LeaderboardDatatable/columns.ts
+++ app/components/LeaderboardDatatable/columns.ts
@@ -1,49 +1,44 @@
-import type { ColumnDef } from "@tanstack/vue-table";
-import { h } from "vue";
-import { ArrowUpDown, ChevronDown } from "lucide-vue-next";
-import { Button } from "~/components/ui/button";
+import type { ColumnDef } from '@tanstack/vue-table';
+import { h } from 'vue';
+import { ArrowUpDown, ChevronDown } from 'lucide-vue-next';
+import { Button } from '~/components/ui/button';
 
-export const columns: ColumnDef<LeaderboardEntry>[] = [
+export const baseColumns: ColumnDef<LeaderboardEntry>[] = [
   {
-    accessorKey: "position",
-    header: () =>
-      h("div", { class: "text-center font-normal text-black" }, "Rank"),
+    accessorKey: 'position',
+    header: () => h('div', { class: 'text-center font-normal text-black' }, 'Rank'),
     cell: ({ row }) => {
-      const position = row.getValue("position") as number;
-      return h(
-        "div",
-        { class: "text-center font-normal text-gray-600" },
-        position.toString()
-      );
+      const position = row.getValue('position') as number;
+      return h('div', { class: 'text-center font-normal text-gray-600' }, position.toString());
     },
   },
   {
-    accessorKey: "nickname",
-    header: () =>
-      h("div", { class: "text-center font-normal text-black" }, "Nickname"),
+    accessorKey: 'nickname',
+    header: () => h('div', { class: 'text-center font-normal text-black' }, 'Nickname'),
     cell: ({ row }) => {
-      const nickname = row.getValue("nickname") as string;
-      return h(
-        "div",
-        { class: "text-center font-normal text-gray-600" },
-        nickname
-      );
+      const nickname = row.getValue('nickname') as string;
+      return h('div', { class: 'text-center font-normal text-gray-600' }, nickname);
     },
   },
   {
-    accessorKey: "score",
-    header: () =>
-      h("div", { class: "text-center font-normal text-black" }, "Score"),
+    accessorKey: 'score',
+    header: () => h('div', { class: 'text-center font-normal text-black' }, 'Score'),
     cell: ({ row }) => {
-      const score = row.getValue("score") as number;
-      return h(
-        "div",
-        { class: "text-center font-normal text-gray-600" },
-        score.toString()
-      );
+      const score = row.getValue('score') as number;
+      return h('div', { class: 'text-center font-normal text-gray-600' }, score.toString());
     },
-  },     
-]
+  },
+];
+
+export const adminColumns: ColumnDef<LeaderboardEntry>[] = [
+  ...baseColumns.slice(0,1),
+  {
+    accessorKey: "studentName",
+    header: () => h("div", { class: "text-center font-normal text-black" }, "Student Name"),
+    cell: ({ row }) => h("div", { class: "text-center text-gray-600" }, row.getValue("studentName")),
+  },
+  ...baseColumns.slice(1),
+];
 
 export interface LeaderboardEntry {
   id: number;
