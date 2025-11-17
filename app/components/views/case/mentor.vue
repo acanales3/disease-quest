db0eacb7e6b61511c4c57c0ee924d37cd1d109eb
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-11-17 23:22:17 +0100

    Add text area component, create response box and output text submitted on question box on response box

--- app/components/views/case/mentor.vue
+++ app/components/views/case/mentor.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <h1>Case Mentor Page</h1>
+        <CaseTextArea />
     </div>
 </template>
 
 <script setup>
-
+import CaseTextArea from '@/components/CaseTextArea/text-area.vue'
 </script>
 
 <style scoped>
