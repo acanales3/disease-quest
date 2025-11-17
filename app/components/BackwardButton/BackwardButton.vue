4f609ee78f5c0cbd7528fa2ff47e04dc8decaa4f
acanales3 <alexander.s.canales@gmail.com>
2025-11-17 15:20:55 -0600

    SCRUM-124: backward and forward component initialization

---
+++ app/components/BackwardButton/BackwardButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ArrowUpIcon } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <Button variant="outline" size="icon" aria-label="Submit">
+    <ArrowUpIcon class="-rotate-90" />
+  </Button>
+</template>
+
+<style scoped></style>
