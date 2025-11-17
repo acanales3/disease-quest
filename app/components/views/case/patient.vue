0720bbd64ffc4a4729c7d6deea412d2818de7cc4
acanales3 <alexander.s.canales@gmail.com>
2025-11-17 16:27:35 -0600

    SCRUM-123: init

--- app/components/views/case/patient.vue
+++ app/components/views/case/patient.vue
@@ -3,12 +3,14 @@
     <h1>Case Patient Page</h1>
     <BackwardButton route="/case/mentor"></BackwardButton>
     <ForwardButton route="/case/evaluation"></ForwardButton>
+    <StepperFlowVisual></StepperFlowVisual>
   </div>
 </template>
 
 <script setup>
 import BackwardButton from "~/components/BackwardButton/BackwardButton.vue";
 import ForwardButton from "~/components/ForwardButton/ForwardButton.vue";
+import StepperFlowVisual from "~/components/StepperFlowVisual/StepperFlowVisual.vue";
 </script>
 
 <style scoped></style>
