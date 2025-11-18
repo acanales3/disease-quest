941c0a832ff969aed82165a2e5121885e6bd675a
Chris Musselman <chrismusselman@Chriss-Air.lan>
2025-11-18 12:06:44 -0600

    adjusted patient view for layout changes

--- app/components/views/case/patient.vue
+++ app/components/views/case/patient.vue
@@ -1,32 +1,20 @@
 <template>
-  <div class="flex justify-center">
-    <BackwardButton route="/case/mentor"></BackwardButton>
-    <ForwardButton route="/case/evaluation"></ForwardButton>
-    <StepperFlowVisual></StepperFlowVisual>
-    <!-- need to add 'Patient and Diagnostic Interactions' to top right of header -->
-    <!-- need to add 'Case Name' above CaseTextArea   -->
-  </div>
-  <div class="flex flex-col items-center">
-    <h1 class="text-lg font-bold pt-8">Patient Agent</h1>
-    <p class="text-md text-muted-foreground ">Discuss with the patient</p>
-  </div>
-  <div class="flex justify-center py-2">
-    <CaseTextArea />
-  </div>
-  <div class="flex justify-center space-x-8 py-20 w-full">
-    <AgentSheet agent="Diagnostic" side="left" />
-    <AgentSheet agent="Mentor" side="right" />
+  <div class="space-y-8 w-full max-w-4xl mx-auto px-4">
+    <div class="flex flex-col items-center w-full">
+      <h1 class="text-lg font-bold pt-12">Patient Agent</h1>
+      <p class="text-md text-muted-foreground ">Discuss with the patient</p>
+      <CaseTextArea class="w-full"/>
+    </div>
+    <div class="flex justify-center space-x-50 py-20 w-full">
+      <AgentSheet agent="Diagnostic" side="left" />
+      <AgentSheet agent="Mentor" side="right" />
+    </div>
   </div>
 </template>
 
 <script setup>
-import BackwardButton from "~/components/BackwardButton/BackwardButton.vue";
-import ForwardButton from "~/components/ForwardButton/ForwardButton.vue";
 import CaseTextArea from "@/components/CaseTextArea/text-area.vue";
 import AgentSheet from "@/components/AgentSheet/AgentSheet.vue";
-import StepperFlowVisual from "~/components/StepperFlowVisual/StepperFlowVisual.vue";
-
-
 </script>
 
 <style scoped></style>
