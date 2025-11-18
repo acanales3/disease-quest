d1c4a50b6c4533b4c841725c728fa252c7d3ece0
Dylan Noall <dylannoall@outlook.com>
2025-11-17 18:42:49 -0600

    FEAT: added validation to 'edit student modal' and fixed infinite MS-Year issue

--- app/components/AdminEditStudentDialog/AdminEditStudentDialog.vue
+++ app/components/AdminEditStudentDialog/AdminEditStudentDialog.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 
 import type { Student } from '~/assets/interface/Student';
 
@@ -26,6 +18,53 @@ const form = ref<Student>({
   msyear: 1,
   classroom: 0,
   status: 'registered',
+});
+
+const errors = ref({
+  name: '',
+  nickname: '',
+  email: '',
+  school: '',
+});
+
+watch(
+  form,
+  (val) => {
+    // name validation
+    if (!val.name) {
+      errors.value.name = 'Name is required.';
+    } else {
+      errors.value.name = '';
+    }
+
+    // nickname validation
+    if (!val.nickname) {
+      errors.value.nickname = 'Nickname is required.';
+    } else {
+      errors.value.nickname = '';
+    }
+
+    // email validation
+    if (!val.email) {
+      errors.value.email = 'Email is required.';
+    } else if (!val.email.endsWith('.edu')) {
+      errors.value.email = 'Email must be a .edu address.';
+    } else {
+      errors.value.email = '';
+    }
+
+    // school validation
+    if (!val.school) {
+      errors.value.school = 'School is required.';
+    } else {
+      errors.value.school = '';
+    }
+  },
+  { deep: true, immediate: true },
+);
+
+const isInvalid = computed(() => {
+  return Object.values(errors.value).some((err) => err !== '');
 });
 
 // when modal opens, populate form inputs with props.data
@@ -67,27 +106,39 @@ const handleSave = () => {
       <div class="flex flex-col gap-3">
         <label class="flex flex-col">
           Name
-          <input type="text" v-model="form.name" class="p-2 border rounded" />
+          <input type="text" v-model="form.name" class="p-2 border rounded" :class="{ 'border-red-500': errors.name }" />
+          <p v-if="errors.name" class="text-red-500 text-xs mt-1">
+            {{ errors.name }}
+          </p>
         </label>
 
         <label class="flex flex-col">
           Nickname
-          <input type="text" v-model="form.nickname" class="p-2 border rounded" />
+          <input type="text" v-model="form.nickname" class="p-2 border rounded" :class="{ 'border-red-500': errors.nickname }" />
+          <p v-if="errors.nickname" class="text-red-500 text-xs mt-1">
+            {{ errors.nickname }}
+          </p>
         </label>
 
         <label class="flex flex-col">
           Email
-          <input type="email" v-model="form.email" class="p-2 border rounded" />
+          <input type="email" v-model="form.email" class="p-2 border rounded" :class="{ 'border-red-500': errors.email }" />
+          <p v-if="errors.email" class="text-red-500 text-xs mt-1">
+            {{ errors.email }}
+          </p>
         </label>
 
         <label class="flex flex-col">
           School
-          <input type="text" v-model="form.school" class="p-2 border rounded" />
+          <input type="text" v-model="form.school" class="p-2 border rounded" :class="{ 'border-red-500': errors.school }" />
+          <p v-if="errors.school" class="text-red-500 text-xs mt-1">
+            {{ errors.school }}
+          </p>
         </label>
 
         <label class="flex flex-col">
           MS Year
-          <input type="number" v-model.number="form.msyear" min="1" class="p-2 border rounded" />
+          <input type="number" v-model.number="form.msyear" min="1" max="4" class="p-2 border rounded" />
         </label>
 
         <label class="flex flex-col">
@@ -104,17 +155,17 @@ const handleSave = () => {
         </label>
       </div>
 
-        <DialogFooter>
-          <button class="p-2 bg-gray-400 transition-colors duration-500 hover:bg-gray-500 rounded text-white"
-            @click="emit('close')">
-            Close
-          </button>
+      <DialogFooter>
+        <button class="p-2 bg-gray-400 transition-colors duration-500 hover:bg-gray-500 rounded text-white" @click="emit('close')">Close</button>
 
-          <button class="p-2 bg-[#AF67F0] transition-colors duration-500 hover:bg-purple-600 rounded text-white"
-            @click="handleSave">
-            Save Changes
-          </button>
-        </DialogFooter>
+        <button
+          class="p-2 bg-[#AF67F0] transition-colors duration-500 hover:bg-purple-600 rounded text-white disabled:opacity-50 disabled:cursor-not-allowed"
+          :disabled="isInvalid"
+          @click="handleSave"
+        >
+          Save Changes
+        </button>
+      </DialogFooter>
     </DialogContent>
   </Dialog>
 </template>
