8d59c451d45c143009a4566759335a2a1c2469d9
Dylan Noall <dylannoall@outlook.com>
2025-11-17 22:28:21 -0600

    STYLE: added styling for mentor chat interface and routing between introduction and patient pages

--- app/components/views/case/mentor.vue
+++ app/components/views/case/mentor.vue
@@ -1,14 +1,30 @@
 <template>
-    <div>
-        <h1>Case Mentor Page</h1>
-        <CaseTextArea />
+  <div class="flex flex-col min-h-screen bg-gray-100">
+    <!-- Top Row -->
+    <div class="w-full flex justify-between p-4">
+      <BackwardButton route="/case/introduction"></BackwardButton>
+      <h1 class="font-semibold text-2xl">Mentor Conversation</h1>
     </div>
+
+    <div>
+      <h1 class="font-semibold text-center text-2xl p-4">{{ caseName }}</h1>
+      <CaseTextArea />
+    </div>
+
+    <div class="mt-auto p-4 flex">
+      <NuxtLink to="/case/patient" class="ml-auto">
+        <button class="rounded-lg shadow-sm px-4 py-2 bg-white transition-colors duration-500 hover:bg-gray-100">Next</button>
+      </NuxtLink>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import CaseTextArea from '@/components/CaseTextArea/text-area.vue'
+import { ref } from 'vue';
+import CaseTextArea from '@/components/CaseTextArea/text-area.vue';
+import BackwardButton from '~/components/BackwardButton/BackwardButton.vue';
+
+const caseName = ref("Case Name");
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
