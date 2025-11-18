80e4722ea1870732d8caba788e05a2b0973b9d84
Dylan Noall <dylannoall@outlook.com>
2025-11-18 16:08:23 -0600

    STYLE: styled the mentor conversation page to look more in-line with the patient conversation page

--- app/components/views/case/mentor.vue
+++ app/components/views/case/mentor.vue
@@ -1,30 +1,16 @@
 <template>
-  <div class="flex flex-col min-h-screen bg-gray-100">
-    <!-- Top Row -->
-    <div class="w-full flex justify-between p-4">
-      <BackwardButton route="/case/introduction"></BackwardButton>
-      <h1 class="font-semibold text-2xl">Mentor Conversation</h1>
-    </div>
-
-    <div>
-      <h1 class="font-semibold text-center text-2xl p-4">{{ caseName }}</h1>
+  <div class="flex flex-col space-y-8 w-full mx-auto px-4">
+    <div class="flex flex-col items-center w-full">
+      <h1 class="text-lg font-bold pt-12">Mentor Agent</h1>
+      <p class="text-md text-muted-foreground">Discuss with your mentor before meeting the patient.</p>
       <CaseTextArea />
-    </div>
-
-    <div class="mt-auto p-4 flex">
-      <NuxtLink to="/case/patient" class="ml-auto">
-        <button class="rounded-lg shadow-sm px-4 py-2 bg-white transition-colors duration-500 hover:bg-gray-100">Next</button>
-      </NuxtLink>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import CaseTextArea from '@/components/CaseTextArea/text-area.vue';
-import BackwardButton from '~/components/BackwardButton/BackwardButton.vue';
 
-const caseName = ref("Case Name");
 </script>
 
 <style scoped></style>
