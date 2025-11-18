b871d1d51aeba1908880c9b55f369473c2bb5cc2
Chris Musselman <chrismusselman@Chriss-Air.lan>
2025-11-17 19:16:51 -0600

    added dependencies for drawer to be used for agent chats

--- app/components/views/case/patient.vue
+++ app/components/views/case/patient.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    <h1>Case Patient Page</h1>
     <BackwardButton route="/case/mentor"></BackwardButton>
     <ForwardButton route="/case/evaluation"></ForwardButton>
+    <!-- need to add 'Patient and Diagnostic Interactions' to top right of header -->
+  </div>
+  <div>
+    <CaseTextArea />
+  </div>
+  <div>
+    <AgentDrawer title="Patient" />
+    <AgentDrawer title="Mentor" />
   </div>
 </template>
 
 <script setup>
 import BackwardButton from "~/components/BackwardButton/BackwardButton.vue";
 import ForwardButton from "~/components/ForwardButton/ForwardButton.vue";
+import CaseTextArea from "@/components/CaseTextArea/text-area.vue";
+import AgentDrawer from "@/components/AgentDrawer/AgentDrawer.vue";
 </script>
 
 <style scoped></style>
