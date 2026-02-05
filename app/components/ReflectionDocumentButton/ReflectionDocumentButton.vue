85259547b37ef0be48981b9a824618130552dff1
acanales3 <alexander.s.canales@gmail.com>
2026-02-04 21:26:45 -0600

    SCRUM-106: initial files

---
+++ app/components/ReflectionDocumentButton/ReflectionDocumentButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+<template>
+  <Button
+    class="px-6 py-8 bg-[#AF67F0] text-white rounded-lg hover:bg-[#AF67F0]/80"
+    aria-label="Forward"
+  >
+    View Takeaway Document
+  </Button>
+</template>
+<style scoped></style>
--- app/components/views/case/evaluation.vue
+++ app/components/views/case/evaluation.vue
@@ -1,3 +1,32 @@
+<script setup lang="ts">
+import CaseTextArea from "@/components/CaseTextArea/text-area.vue";
+
+import ClassroomScores from "../../graph/ScoresByCategoryGraph.vue";
+import type { CaseScore } from "@/components/AssessmentCategoryScoreGraph/types";
+import ReflectionDocumentButton from "../../ReflectionDocumentButton/ReflectionDocumentButton.vue";
+
+import TotalCount from "~/components/ui/TotalCount.vue";
+
+const mockData: CaseScore[] = [
+  { case: "Case 1", score: 85 },
+  { case: "Case 2", score: 90 },
+  { case: "Case 3", score: 75 },
+  { case: "Case 4", score: 95 },
+  { case: "Case 5", score: 80 },
+  { case: "Case 6", score: 75 },
+  { case: "Case 7", score: 95 },
+  { case: "Case 8", score: 80 },
+  { case: "Case 9", score: 95 },
+  { case: "Case 10", score: 80 },
+  { case: "Case 11", score: 75 },
+  { case: "Case 12", score: 95 },
+  { case: "Case 13", score: 80 },
+];
+
+const index: keyof CaseScore = "case";
+const categories: (keyof CaseScore)[] = ["score"];
+</script>
+
 <template>
   <div class="flex flex-col w-full mx-auto px-6 mb-20 space-y-10">
     <div class="flex flex-col items-center w-full pt-10 space-y-8">
@@ -30,36 +59,9 @@
       </div>
 
       <CaseTextArea />
+      <ReflectionDocumentButton />
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-import CaseTextArea from "@/components/CaseTextArea/text-area.vue";
-
-import ClassroomScores from "../../graph/ScoresByCategoryGraph.vue";
-import type { CaseScore } from "@/components/AssessmentCategoryScoreGraph/types";
-
-import TotalCount from "~/components/ui/TotalCount.vue";
-
-const mockData: CaseScore[] = [
-  { case: "Case 1", score: 85 },
-  { case: "Case 2", score: 90 },
-  { case: "Case 3", score: 75 },
-  { case: "Case 4", score: 95 },
-  { case: "Case 5", score: 80 },
-  { case: "Case 6", score: 75 },
-  { case: "Case 7", score: 95 },
-  { case: "Case 8", score: 80 },
-  { case: "Case 9", score: 95 },
-  { case: "Case 10", score: 80 },
-  { case: "Case 11", score: 75 },
-  { case: "Case 12", score: 95 },
-  { case: "Case 13", score: 80 },
-];
-
-const index: keyof CaseScore = "case";
-const categories: (keyof CaseScore)[] = ["score"];
-</script>
 
 <style scoped></style>
